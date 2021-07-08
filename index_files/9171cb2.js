b45c8fd38179eaa0b1de7c779fb588c69dd29365
Anthony Ngo <anthonyngo002@gmail.com>
2021-07-08 15:09:07 -0700

    ahhhhhhh

---
+++ index_files/9171cb2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{165:function(n,o,w){w(166),n.exports=w(167)}},[[165,3,1,4]]]);
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <NotionRenderer :blockMap="blockMap" fullPage />
+</template>
+
+<script>
+export default {
+  data: () => ({ blockMap: null }),
+  async asyncData({ $notion }) {
+    // use Notion module to get Notion blocks from the API via a Notion pageId
+    const blockMap = await $notion.getPageBlocks(
+      "c436f8eb0b624830be4f9341727da64a"
+    );
+    return { blockMap };
+  },
+}
+</script>
