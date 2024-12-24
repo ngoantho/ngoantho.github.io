fdf7a8c1f60b99ad74f79a3b8d130d1787e90f4b
Anthony Ngo <anthony.ngo.dev@gmail.com>
2024-12-24 13:02:37 -0800

    rename resume.pdf to portfolio.pdf

--- watcher.py
+++ watcher.py
@@ -8,12 +8,12 @@ class Handler(FileSystemEventHandler):
     def on_modified(self, event):
         if not isinstance(event, FileModifiedEvent):
             return
-        if not event.src_path == ".\\resume.odp":
+        if not event.src_path == ".\\portfolio.odp":
             return
 
-        print("Converting resume.odp to resume.pdf")
+        print("Converting portfolio.odp to portfolio.pdf")
         try:
-            run(["soffice.exe", "--headless", "--convert-to", "pdf", "resume.odp"])
+            run(["soffice.exe", "--headless", "--convert-to", "pdf", "portfolio.odp"])
         except Exception as e:
             print(f"Error: {e}")
         print("Conversion complete")
